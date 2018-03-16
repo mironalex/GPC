0f688513a3000bb320a165a9fd6be9deee5fae91
Valentin-Bogdan Roșca <rosca.valentin2012@gmail.com>
2018-03-16 14:58:54 +0200

    Use global defines for grid and ASD3 algo.

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -1,9 +1,18 @@
 #include <cstdlib>
 
 #include <GL/glut.h>
+#include <utility>
+#include <vector>
 
 // dimensiunea ferestrei in pixeli
 #define dim 300
+
+#define GRID_NUM_LINES 20
+#define GRID_NUM_COLUMNS 20
+#define GRID_VIEWPORT_START_X (-0.98)
+#define GRID_VIEWPORT_START_Y (-0.98)
+#define GRID_VIEWPORT_SIZE_X 1.96
+#define GRID_VIEWPORT_SIZE_Y 1.96
 
 
 void Init(void) {
@@ -15,6 +24,57 @@ void Init(void) {
     //glPointSize(3);
 
     glPolygonMode(GL_FRONT, GL_LINE);
+}
+
+double getViewportCoordOnGrid(int axis_size,
+                              double viewport_offset,
+                              double viewport_grid_size,
+                              int point_coord) {
+    double quotent = (double)point_coord / axis_size;
+    return viewport_offset + quotent * viewport_grid_size;
+}
+
+void drawGlVertexAtGridCoords(int x, int y) {
+    double viewport_x = getViewportCoordOnGrid(
+            GRID_NUM_LINES,
+            GRID_VIEWPORT_START_X,
+            GRID_VIEWPORT_SIZE_X,
+            x
+    );
+
+    double viewport_y = getViewportCoordOnGrid(
+            GRID_NUM_COLUMNS,
+            GRID_VIEWPORT_START_Y,
+            GRID_VIEWPORT_SIZE_Y,
+            y
+    );
+
+    glVertex2d(viewport_x, viewport_y);
+}
+
+void DrawLine(int start_x, int start_y, int end_x, int end_y, int lines, int columns) {
+    int dx = 2, dy = 3;
+
+    int d = 2 * dy - dx;
+    int dE = 2 * dy;
+    int dNE = 2 * (dy - dx);
+    int x = start_x, y = start_y;
+
+    glBegin(GL_POINTS);
+    drawGlVertexAtGridCoords(x, y);
+    while (x < end_x)
+    {
+        if (d <= 0) {
+            d += dE;
+            x++;
+        }
+        else
+        {
+            d += dNE; x++; y++;
+        }
+        drawGlVertexAtGridCoords(x, y);;
+    }
+    glEnd();
 }
 
 
@@ -30,19 +90,31 @@ void DrawGrid(int lines, int columns) {
 
     // Draw lines
     for(int l = 0; l <= lines; l++) {
-        double line_quotent = (double)l / lines;
+        double viewport_x = getViewportCoordOnGrid(
+            GRID_NUM_LINES,
+            GRID_VIEWPORT_START_X,
+            GRID_VIEWPORT_SIZE_X,
+            l
+        );
+
         glBegin(GL_LINES);
-        glVertex2d(offset_x + line_quotent * grid_size_x, offset_y);
-        glVertex2d(offset_x + line_quotent * grid_size_x, offset_y + grid_size_y);
+        glVertex2d(viewport_x, offset_y);
+        glVertex2d(viewport_x, offset_y + grid_size_y);
         glEnd();
     }
 
     // Draw columns
     for(int c = 0; c <= columns; c++) {
-        double column_quotent = (double)c / columns;
+        double viewport_y = getViewportCoordOnGrid(
+            GRID_NUM_COLUMNS,
+            GRID_VIEWPORT_START_Y,
+            GRID_VIEWPORT_SIZE_Y,
+            c
+        );
+
         glBegin(GL_LINES);
-        glVertex2d(offset_x, offset_y + column_quotent * grid_size_y);
-        glVertex2d(offset_x + grid_size_x, offset_y + column_quotent * grid_size_y);
+        glVertex2d(offset_x,viewport_y);
+        glVertex2d(offset_x + grid_size_x, viewport_y);
         glEnd();
     }
 }
