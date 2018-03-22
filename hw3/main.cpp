688d32a5b96ef212d9d0d2cd37f1b725b200da36
Valentin-Bogdan Roșca <rosca.valentin2012@gmail.com>
2018-03-22 14:24:39 +0200

    Add class grid with necessary methods.

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -3,16 +3,134 @@
 #include <GL/glut.h>
 #include <utility>
 #include <vector>
+#include <tuple>
 
 // dimensiunea ferestrei in pixeli
 #define dim 300
 
-#define GRID_NUM_LINES 20
-#define GRID_NUM_COLUMNS 20
-#define GRID_VIEWPORT_START_X (-0.98)
-#define GRID_VIEWPORT_START_Y (-0.98)
-#define GRID_VIEWPORT_SIZE_X 1.96
-#define GRID_VIEWPORT_SIZE_Y 1.96
+
+class Grid {
+    int num_lines_;
+    int num_columns_;
+    double viewport_start_x_;
+    double viewport_start_y_;
+    double viewport_size_x_;
+    double viewport_size_y_;
+
+    double static GetViewportCoordOnGrid(int axis_size,
+                                         double viewport_offset,
+                                         double viewport_grid_size,
+                                         int point_coord) {
+        double quotent = (double)point_coord / axis_size;
+        return viewport_offset + quotent * viewport_grid_size;
+    }
+
+    void DrawCircle(int x, int y) {
+        double viewport_x, viewport_y;
+        std::tie(viewport_x, viewport_y) = this->GetViewportFromInteger(x, y);
+
+        // TODO: Draw the circle here -> Alex
+        glPointSize(2);
+        glBegin(GL_POINTS);
+        glVertex2d(viewport_x, viewport_y);
+        glEnd();
+    }
+
+public:
+    std::pair<double, double> GetViewportFromInteger(int x, int y) {
+        double viewport_x = GetViewportCoordOnGrid(
+                this->num_lines_,
+                this->viewport_start_x_,
+                this->viewport_size_x_,
+                x
+        );
+
+        double viewport_y = GetViewportCoordOnGrid(
+                this->num_columns_,
+                this->viewport_start_y_,
+                this->viewport_size_y_,
+                y
+        );
+
+        return std::make_pair(viewport_x, viewport_y);
+    }
+
+    Grid(int num_lines, int num_columns,
+         double viewport_start_x, double viewport_start_y,
+         double viewport_size_x, double viewport_size_y) {
+
+        this->num_lines_ = num_lines;
+        this->num_columns_ = num_columns;
+        this->viewport_start_x_ = viewport_start_x;
+        this->viewport_start_y_ = viewport_start_y;
+        this->viewport_size_x_ = viewport_size_x;
+        this->viewport_size_y_ = viewport_size_y;
+    }
+
+    void DrawLineOnSelf(int start_x, int start_y, int end_x, int end_y) {
+        int dx = 2, dy = 3;
+
+        int d = 2 * dy - dx;
+        int dE = 2 * dy;
+        int dNE = 2 * (dy - dx);
+        int x = start_x, y = start_y;
+
+        this->DrawCircle(x, y);
+        while (x < end_x)
+        {
+            if (d <= 0) {
+                d += dE;
+                x++;
+            }
+            else
+            {
+                d += dNE; x++; y++;
+            }
+            this->DrawCircle(x, y);
+        }
+    }
+
+    void DrawSelf() {
+        // Viewport offsets
+        double offset_x = -0.98;
+        double offset_y = -0.98;
+
+        double grid_size_x = 1.96;
+        double grid_size_y = 1.96;
+
+        glColor3d(0.5, 0.5, 0.5);
+
+        // Draw lines
+        for(int l = 0; l <= this->num_lines_; l++) {
+            double viewport_x = GetViewportCoordOnGrid(
+                    this->num_lines_,
+                    this->viewport_start_x_,
+                    this->viewport_size_x_,
+                    l
+            );
+
+            glBegin(GL_LINES);
+            glVertex2d(viewport_x, offset_y);
+            glVertex2d(viewport_x, offset_y + grid_size_y);
+            glEnd();
+        }
+
+        // Draw columns
+        for(int c = 0; c <= this->num_columns_; c++) {
+            double viewport_y = GetViewportCoordOnGrid(
+                    this->num_columns_,
+                    this->viewport_start_y_,
+                    this->viewport_size_y_,
+                    c
+            );
+
+            glBegin(GL_LINES);
+            glVertex2d(offset_x,viewport_y);
+            glVertex2d(offset_x + grid_size_x, viewport_y);
+            glEnd();
+        }
+    }
+};
 
 
 void Init(void) {
@@ -26,102 +144,14 @@ void Init(void) {
     glPolygonMode(GL_FRONT, GL_LINE);
 }
 
-double getViewportCoordOnGrid(int axis_size,
-                              double viewport_offset,
-                              double viewport_grid_size,
-                              int point_coord) {
-    double quotent = (double)point_coord / axis_size;
-    return viewport_offset + quotent * viewport_grid_size;
-}
-
-void drawGlVertexAtGridCoords(int x, int y) {
-    double viewport_x = getViewportCoordOnGrid(
-            GRID_NUM_LINES,
-            GRID_VIEWPORT_START_X,
-            GRID_VIEWPORT_SIZE_X,
-            x
-    );
-
-    double viewport_y = getViewportCoordOnGrid(
-            GRID_NUM_COLUMNS,
-            GRID_VIEWPORT_START_Y,
-            GRID_VIEWPORT_SIZE_Y,
-            y
-    );
-
-    glVertex2d(viewport_x, viewport_y);
-}
-
-void DrawLine(int start_x, int start_y, int end_x, int end_y, int lines, int columns) {
-    int dx = 2, dy = 3;
-
-    int d = 2 * dy - dx;
-    int dE = 2 * dy;
-    int dNE = 2 * (dy - dx);
-    int x = start_x, y = start_y;
-
-    glBegin(GL_POINTS);
-    drawGlVertexAtGridCoords(x, y);
-    while (x < end_x)
-    {
-        if (d <= 0) {
-            d += dE;
-            x++;
-        }
-        else
-        {
-            d += dNE; x++; y++;
-        }
-        drawGlVertexAtGridCoords(x, y);;
-    }
-    glEnd();
-}
-
-
-void DrawGrid(int lines, int columns) {
-    // Viewport offsets
-    double offset_x = -0.98;
-    double offset_y = -0.98;
-
-    double grid_size_x = 1.96;
-    double grid_size_y = 1.96;
-
-    glColor3d(0.5, 0.5, 0.5);
-
-    // Draw lines
-    for(int l = 0; l <= lines; l++) {
-        double viewport_x = getViewportCoordOnGrid(
-            GRID_NUM_LINES,
-            GRID_VIEWPORT_START_X,
-            GRID_VIEWPORT_SIZE_X,
-            l
-        );
-
-        glBegin(GL_LINES);
-        glVertex2d(viewport_x, offset_y);
-        glVertex2d(viewport_x, offset_y + grid_size_y);
-        glEnd();
-    }
-
-    // Draw columns
-    for(int c = 0; c <= columns; c++) {
-        double viewport_y = getViewportCoordOnGrid(
-            GRID_NUM_COLUMNS,
-            GRID_VIEWPORT_START_Y,
-            GRID_VIEWPORT_SIZE_Y,
-            c
-        );
-
-        glBegin(GL_LINES);
-        glVertex2d(offset_x,viewport_y);
-        glVertex2d(offset_x + grid_size_x, viewport_y);
-        glEnd();
-    }
-}
 
 void DisplaySolutions(void){
     glClear(GL_COLOR_BUFFER_BIT);
-    DrawGrid(20, 20);
+    Grid *g = new Grid(20, 20, -0.98, -0.98, 1.96, 1.96);
+
+    g->DrawSelf();
+    g->DrawLineOnSelf(15, 15, 10, 10);
+
     glFlush();
 }
 
